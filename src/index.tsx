5226e126badd40a4098f89ac5ac482cb955484dd
Yuliia Antonenko <shinjisan311@gmail.com>
2023-12-03 15:06:47 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { App } from './App.tsx';
 import reportWebVitals from './reportWebVitals';
+import BrowserRouter from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
